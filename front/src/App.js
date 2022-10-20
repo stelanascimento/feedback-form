1cdcffa06248118f92430d88e0a3fd9e86933650
Stela <stelaoliver2020@gmaiil.com>
2022-10-19 23:56:18 -0300

    updates

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,142 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import rosa from "./assets/pink.png";
+import ghost from "./assets/green.png";
+import yellow from "./assets/Vector.png";
+import world from "./assets/print.png";
+import {
+  FaLinkedinIn,
+  FaPinterestP,
+  FaFacebookF,
+  FaTwitter,
+} from "react-icons/fa";
 
+//import axios from "axios";
 function App() {
+  const [dataForm, setDataForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [response, setResponse] = useState({
+    type: "",
+    mensagem: "",
+  });
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    console.log(dataForm.email);
+
+    try {
+      const res = await fetch("http://localhost:8888/show", {
+        method: "POST",
+        body: JSON.stringify(dataForm),
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          "Access-Control-Allow-Origin": "*",
+        },
+      });
+
+      const responseEnv = await res.json();
+
+      if (responseEnv.erro) {
+        setResponse({
+          type: "error",
+          mensagem: responseEnv.mensagem,
+        });
+      } else {
+        setResponse({
+          type: "success",
+          mensagem: responseEnv.mensagem,
+        });
+
+        setDataForm({
+          name: "",
+          email: "",
+          message: "",
+        });
+      }
+    } catch (err) {
+      setResponse({
+        type: "error",
+        mensagem: "Error: Try later !",
+      });
+    }
+  };
+
+  // function handleSubmit(e) {
+  //   e.preventDefault(); //Sempre usar o preventDefault
+  //   //console.log(info);
+  // }
+
+  function handleChange({ target }) {
+    console.log(target.value);
+    setDataForm({ ...dataForm, [target.name]: target.value });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="dad-card">
+        <div className="child-1">
+          <div className="feedback ">
+            <h1>Reach out us!</h1>
+            <form onSubmit={handleRegister}>
+              <div>
+                <input
+                  id="name"
+                  type="text"
+                  name="name"
+                  value={dataForm.name}
+                  onChange={handleChange}
+                  placeholder="Your name*"
+                />
+              </div>
+              {/* O conceito do input controlado eh quando o input vai ser controlado por um estado. */}
+              <div>
+                <input
+                  id="email"
+                  type="email"
+                  name="email"
+                  value={dataForm.email}
+                  placeholder="Your e-mail*"
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <input
+                  id="message"
+                  type="text"
+                  name="message"
+                  value={dataForm.message}
+                  placeholder="Your message*"
+                  onChange={handleChange}
+                />
+              </div>
+              <button type="submit">Send message</button>
+            </form>
+          </div>
+        </div>
+
+        <div className="child-2">
+          <img src={world} alt="Ghost" className="world" />
+        </div>
+      </div>
+
+      <div className="footer">
+        <img src={rosa} />
+        <div className="socialMedia">
+          <FaLinkedinIn size="1rem" color="#696969" />
+          <FaTwitter size="1rem" color="#696969" />
+          <FaFacebookF size="1rem" color="#696969" />
+          <FaPinterestP size="1rem" color="#696969" />
+        </div>
+        <div className="little-ghost">
+          <img src={ghost} />
+        </div>
+        <div className="little-ghost">
+          <img src={yellow} />
+        </div>
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -4,17 +4,15 @@ const app = express(); //vai executar o express
 const cors = require("cors");
 
 app.use(express.json());
-app.use(routes);
 
 app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "X-PINGOTHER, Content-Type, Authorization"
-  );
+  res.header("Access-Control-Allow-Headers", "*");
   app.use(cors());
   next();
 });
+app.use(routes);
 
 app.listen(8888);
